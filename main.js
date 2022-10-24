4e602fa07c933f80d233fb6e1e809250fed0e5fe
ayana-ismagambetova <ayana.ismagambetova@nu.edu.kz>
2022-10-24 14:18:11 +0600

    main.js file modified

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*task1*/
+/*task#6*/
 let a=2
 let b=3
 console.log("first number is "+a);
@@ -8,28 +8,32 @@ function squares(a,b){
 }
 console.log(squares(2,3));
 
-/*task2*/
+/*task#11*/
 let c=11;
 console.log("the given number is "+c);
 function oddeven(c){
 	d=c%2;
 	if (d=0){
-		console.log("the given number is even")
+		console.log("the given number is even");
 	}
 	else{
-		console.log("the given number is odd")
+		console.log("the given number is odd");
 	}
 }
 console.log(oddeven(11));
 
 
-/*task3*/
+/*task#14*/
 let e=5;
 let f=12;
-console.log("the given two numbers are "+e,f);
-if(e>f){
-	console.log("the greatest number is "+e)
-	}
-else{
-	console.log("the greatest number is "+f)
+let g=30;
+console.log("the given two numbers are "+e,f,g);
+if(e>f && e>g){
+	console.log("the greatest number is "+e);
 }
+else if(f>e && f>g){
+	console.log("the greatest number is "+f);
+}
+else{
+	console.log("the greatest number is "+g)
+};
